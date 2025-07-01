8243a9eff0d613ce538d3918c174f00f07cf41de
AlekseyZhivoderov <azhivoderov@yandex.ru>
2025-07-02 01:24:25 +0500

    Task10

---
+++ src/itmo/java/labs/task10/Main.java
@@ -0,0 +1,7 @@
+package itmo.java.labs.task10;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/itmo/java/labs/task10/ReadWriteFile.java
@@ -0,0 +1,13 @@
+package itmo.java.labs.task10;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ReadWriteFile {
+    public Set<String> readFile(){
+
+        return new HashSet<>();
+    }
+}
