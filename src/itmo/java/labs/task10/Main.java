88ce68ab329898cca97e650b5d69e58ac0da0fa7
AlekseyZhivoderov <azhivoderov@yandex.ru>
2025-07-03 02:51:33 +0500

    Task10 - 2

--- src/itmo/java/labs/task10/Main.java
+++ src/itmo/java/labs/task10/Main.java
@@ -1,7 +1,17 @@
 package itmo.java.labs.task10;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        //1
+        File file = new File("file1.txt");
+        System.out.println(ReadWriteFile.readFile(file));
+
+        //2
+        ReadWriteFile.addFile(file, "123");
+
+        //3
 
     }
 }
--- src/itmo/java/labs/task10/ReadWriteFile.java
+++ src/itmo/java/labs/task10/ReadWriteFile.java
@@ -1,13 +1,41 @@
 package itmo.java.labs.task10;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class ReadWriteFile {
-    public Set<String> readFile(){
+import static java.awt.SystemColor.text;
 
-        return new HashSet<>();
+public class ReadWriteFile {
+
+    public static Set<String> readFile(File file){
+        String str;
+
+        if(!file.exists() && file.isDirectory()) {
+            return null;
+        }
+
+        Set<String> strings = new LinkedHashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            while ((str = reader.readLine()) != null){
+                strings.add(str);
+            }
+            return strings;
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+            return null;
+        }
     }
+
+    public static void addFile(File file, String text){
+        if(file.exists() && !file.isDirectory()) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))){
+                writer.append(text);
+            }catch (IOException e){
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
 }
